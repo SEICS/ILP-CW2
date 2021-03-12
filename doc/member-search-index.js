dcc1f68bd6ad17f4e950eca12a78eecb6815b2ed
SEICS <52275062+SEICS@users.noreply.github.com>
2021-03-12 04:13:34 +0000

    Add files via upload

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"uk.ac.ed.inf.aqmaps","c":"W3Wdetails","l":"getCoordinates()"},{"p":"uk.ac.ed.inf.aqmaps","c":"W3Wdetails","l":"setCoordinates(Coordinates)","url":"setCoordinates(uk.ac.ed.inf.aqmaps.Coordinates)"},{"p":"uk.ac.ed.inf.aqmaps","c":"W3Wdetails","l":"toString()"},{"p":"uk.ac.ed.inf.aqmaps","c":"W3Wdetails","l":"W3Wdetails()","url":"%3Cinit%3E()"}]
